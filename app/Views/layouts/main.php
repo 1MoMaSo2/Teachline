1d97ff2863ab3a9f6f82fb17d1b6d002bcf1c5f4
MohammadMahdi Soveyzi <mohammadmahdi.soveyzi.dev@gmail.com>
2026-09-11 00:20:11 +0330

    Fix home page statistics counter

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -11,5 +11,6 @@
 
 <?php require_once __DIR__ . '/../partials/footer.php'; ?>
 <script src="/teachline/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="/teachline/assets/vendor/purecounterjs/dist/purecounter_vanilla.js"></script>
 </body>
 </html>
