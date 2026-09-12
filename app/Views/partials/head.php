ace9f2d0996b51a866b07d9f866b9fcee1a754c6
MohammadMahdi Soveyzi <mohammadmahdi.soveyzi.dev@gmail.com>
2026-09-12 10:58:14 +0330

    Add SweetAlert2 to shared view head

--- app/Views/partials/head.php
+++ app/Views/partials/head.php
@@ -79,6 +79,6 @@
 
     <!-- Theme CSS -->
     <link rel="stylesheet" type="text/css" href="/teachline/assets/css/style-rtl.css">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    <script src="https://cdn.jsdeliver.net/npm/sweetalert2@11"></script>
 
 </head>
