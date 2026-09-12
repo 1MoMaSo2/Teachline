7f8f9ea8691e789c26322a69c9c34d917d67fb1d
MohammadMahdi Soveyzi <mohammadmahdi.soveyzi.dev@gmail.com>
2026-09-12 11:01:12 +0330

    Fix SweetAlert2 CDN and preserve Toastr

--- app/Views/partials/head.php
+++ app/Views/partials/head.php
@@ -79,6 +79,7 @@
 
     <!-- Theme CSS -->
     <link rel="stylesheet" type="text/css" href="/teachline/assets/css/style-rtl.css">
-    <script src="https://cdn.jsdeliver.net/npm/sweetalert2@11"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 </head>
