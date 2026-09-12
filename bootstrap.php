b297d5c9887afc0db9e8ba9466ac1a28677b227d
MohammadMahdi Soveyzi <mohammadmahdi.soveyzi.dev@gmail.com>
2026-09-12 11:11:02 +0330

    Load flash helper in application bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -14,6 +14,7 @@ use App\Controllers\AuthController;
 
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/app/Helpers/url.php';
+require_once __DIR__ . '/app/Helpers/flash.php';
 
 $database = new Database();
 $connection = $database->getConnection();
